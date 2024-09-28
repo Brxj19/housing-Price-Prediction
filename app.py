324e2c146194a4cd26f243179406c29de4964c3d
Brxj19 <kumarbraj1925@gmail.com>
2024-09-28 15:18:47 +0530

    third commit

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ regmodel = pickle.load(open('regmodel.pkl','rb'))
 scalar = pickle.load(open('scaling.pkl','rb'))
 @app.route('/')
 def home():
-    return render_template(('home.html'))
+    return render_template(('index.html'))
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
@@ -21,6 +21,16 @@ def predict_api():
     print(output[0])
     
     return jsonify(output[0])
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+
+    return render_template("index.html", prediction_text= "The predicted price is ${}k".format(output))
 
 if __name__ == "__main__":
     app.run(debug = True)
